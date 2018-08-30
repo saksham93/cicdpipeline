5a77397c45212d37e8300278ed8a0a1ef6c1f7d7
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-08-30 21:04:01 +0530

    Updated

---
+++ src/test/java/com/nagarro/cicdpipeline/TestClass.java
@@ -0,0 +1,13 @@
+package com.nagarro.cicdpipeline;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass {
+	
+	@Test
+	public void testMethod() {
+		Assert.assertEquals(1, 1);
+	}
+
+}
